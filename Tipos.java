57420001300ab814d36660be7a0c2b1bc695f0c8
dominiknovicky <dominik.novicky24@gmail.com>
2018-01-30 13:14:45 +0100

    commit

--- Tipos.java
+++ Tipos.java
@@ -1,35 +1,62 @@
 package sk.akademiasovy.tipos;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Tipos {
 
     private int[] arr;
 
-    public int[] getArr(){
+    public int[] getArr() {
         return arr;
     }
 
-    public Tipos (){
+    public Tipos() {
         arr = new int[5];
     }
 
-    public void generate(){
+    public void generate() {
         Random random = new Random();
-        arr[0] = random.nextInt(35)+1;
-        int i = 0;
-        boolean unq = true;
+        arr[0] = random.nextInt(35) + 1;
+        int j,i = 1;
+        boolean unique = true;
 
-        while(i < 5){
-            for (int j = 1; j < arr.length; j++) {
-                if(arr[i] == arr[j])
-                    arr[j] = random.nextInt(35)+1;
+        while (i < 5) {
+            arr[i] = random.nextInt(35) + 1;
+            unique = true;
+            for (j = 0; j < i; j++) {
+                if (arr[i] == arr[j])
+                    unique = false;
+
             }
-            i++;
+
+            if (unique == true)
+                i++;
         }
+        Arrays.sort(arr);
     }
 
-    public void print(){
+    /*public void generate(){
+        Random rand = new Random();
+        arr[0] = rand.nextInt(35)+1;
+        int i = 1;
+        boolean unq;
+
+        while (i < 5) {
+            arr[i] = rand.nextInt(35)+1;
+            unq = true;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j])
+                    unq = false;
+            }
+            if(unq == true)
+                i++;
+        }
+        Arrays.sort(arr);
+    }*/
+
+
+    public void print (){
         for (int j = 0; j < arr.length; j++) {
             System.out.println(arr[j]);
         }
--- database/MySQLdatabase.java
+++ database/MySQLdatabase.java
@@ -1,4 +1,53 @@
 package sk.akademiasovy.tipos.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MySQLdatabase {
+    private final String url = "jdbc:mysql://localhost:3306/";
+    private final String dbName = "tipos";
+    private final String driver = "com.mysql.jdbc.Driver";
+    private final String userName = "user2";
+    private final String password = "secret";
+    private Connection conn;
+
+    public void testConnection() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        try {
+            Class.forName(driver).newInstance();
+            conn = DriverManager.getConnection(url + dbName, userName, password);
+            if (conn == null)
+                System.out.println("Connection failed.");
+            else {
+                System.out.println("Connection OK.");
+            }
+            conn.close();
+        }
+        catch (Exception e){
+            System.out.println("Error, I cannot connect to the database");
+        }
+    }
+
+    public boolean insertValuesIntoDrawHistory(int arr[]) throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        try {
+            Class.forName(driver).newInstance();
+            conn = DriverManager.getConnection(url + dbName, userName, password);
+            String cmd = "INSERT INTO draw_history(ball1, ball2, ball3, ball4, ball5)";
+            cmd+=" VALUES(?,?,?,?,?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(cmd);
+            preparedStatement.setInt(1,arr[0]);
+            preparedStatement.setInt(2,arr[1]);
+            preparedStatement.setInt(3,arr[2]);
+            preparedStatement.setInt(4,arr[3]);
+            preparedStatement.setInt(5,arr[4]);
+            preparedStatement.executeUpdate();
+            conn.close();
+        }
+
+        catch (Exception e){
+            System.out.println("Error, I cannot connect to the database");
+        }
+        return true;
+    }
 }
